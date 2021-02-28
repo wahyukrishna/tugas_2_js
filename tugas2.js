3b3dadcf1918994fae38e83f61f044bc9260c9e9
Wahyu Krishna <hanzo180895@gmail.com>
2021-02-28 22:55:43 +0700

    Tugas 2 JS
    Tugas Niomic

---
+++ tugas2.js
@@ -0,0 +1,10 @@
+var angka = 18;
+var huruf = "Javascript";
+var option = false;
+option = !false;
+var kosong = 0;
+kosong = null;
+var unDefined;
+var array1 = [1,2,3,4,5];
+
+console.log(angka,"\n",huruf,"\n",option,"\n",kosong,"\n",unDefined,"\n",array1);
